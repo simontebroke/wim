7b54616d4293971a470e164b73518deff09f9fd4
Lennart Opalka <139589969+opalkal@users.noreply.github.com>
2025-04-26 22:57:43 +0200

    j

--- src/Site.jsx
+++ src/Site.jsx
@@ -44,8 +44,8 @@ export default function Site() {
   };
 
   return (
-    <div className="overflow-hidden bg-gray-950 min-h-screen flex flex-col items-center justify-start pt-22 sm:justify-center sm:pt-0 relative isolate">
-      <div
+    <div className="overflow-hidden bg-black min-h-screen flex flex-col items-center justify-start pt-22 sm:justify-center sm:pt-0 relative isolate">
+      {/* <div
         aria-hidden="true"
         className="absolute inset-x-0 -top-40 -z-10 transform-gpu overflow-hidden blur-3xl sm:-top-80 opacity-60 sm:opacity-30"
       >
@@ -56,9 +56,9 @@ export default function Site() {
           }}
           className="relative left-[calc(50%-11rem)] aspect-1155/678 w-[36.125rem] -translate-x-1/2 rotate-[30deg] bg-linear-to-tr from-[#ff80b5] to-[#9089fc] opacity-30 sm:left-[calc(50%-30rem)] sm:w-[72.1875rem]"
         />
-      </div>
+      </div> */}
 
-      <div
+      {/* <div
         aria-hidden="true"
         className="absolute inset-x-0 top-[calc(100%-13rem)] -z-10 transform-gpu overflow-hidden blur-3xl sm:top-[calc(100%-30rem)] opacity-50 sm:opacity-"
       >
@@ -69,7 +69,7 @@ export default function Site() {
           }}
           className="relative left-[calc(50%+3rem)] aspect-1155/678 w-[36.125rem] -translate-x-1/2 bg-linear-to-tr from-[#ff80b5] to-[#9089fc] opacity-50 sm:opacity-30 sm:left-[calc(50%+36rem)] sm:w-[72.1875rem]"
         />
-      </div>
+      </div> */}
 
       {!isExerciseActive ? (
         <div className="relative isolate px-6 lg:px-8 w-full max-w-4xl text-center z-10">
